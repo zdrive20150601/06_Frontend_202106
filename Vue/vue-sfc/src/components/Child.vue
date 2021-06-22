03d89068a80f61185fd579d1fd3306ca31d8eb1e
arihito <arihito.m@gmail.com>
2021-06-22 14:04:02 +0900

    add: vue-sfc

---
+++ Vue/vue-sfc/src/components/Child.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <p>カウント数: {{ count }}</p>
+    </div>
+</template>
+
+<script>
+import { defineComponent, computed } from 'vue'
+
+export default defineComponent({
+    name: 'Child',
+    props: {
+        message: {
+            type: String,
+            required: true
+        },
+        count: {
+            type: Number,
+            default: 0
+        }
+    },
+    setup (props){
+        const count = computed(() => props.count * 2)
+        return { count }
+    }
+})
+</script>
---
+++ Vue/vue-sfc/src/components/Parent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <Child :message="title" :count="state.num" />
+        <button @click="countNum">2倍</button>
+    </div>
+</template>
+
+<script>
+import { defineComponent, computed, reactive, ref } from 'vue'
+import Child from './Child.vue'
+
+export default defineComponent ({
+    name: 'Parent',
+    components: { Child },
+    setup () {
+        const title = ref('タイトル')
+        const state = reactive({
+                num: 1,
+                numState: computed(() => state.num * 2 )
+            })
+        const countNum = () => state.num++
+        console.log(state.num)
+        return { title, state, countNum, console }
+    }
+})
+</script>
