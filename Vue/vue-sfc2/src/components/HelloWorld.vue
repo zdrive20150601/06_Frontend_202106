da74c5290adbd0ede85669d202f430813cea73a2
arihito <arihito.m@gmail.com>
2021-06-22 14:41:29 +0900

    add: vue-sfc2

---
+++ Vue/vue-sfc2/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <RefLinks />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import RefLinks from './RefLinks.vue'
+
+export default defineComponent({
+  name: 'HelloWorld',
+  components: { RefLinks },
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
